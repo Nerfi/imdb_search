d006d72e9d0fe658ead97a3d8cb8b0d9cc8a0bbc
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-26 16:46:47 +0100

    added ILIKE to search, working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,27 @@
 class MoviesController < ApplicationController
+#con este codigo  encontraremos movies by title
+ #de But super wont return Superman Returns
   def index
+    if params[:query].present?
+      @movies = Movie.where(title: params[:query])
+    else
     @movies = Movie.all
 
+    end
+  end
+end
+
+
+#Where ILIKE
+#We want super to return movies with Superman in its title:
+
+
+class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%") # esa condicion la conseguimos con esta linea
+    else
+      @movies = Movie.all
+    end
   end
 end
