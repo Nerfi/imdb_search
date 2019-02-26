b6547d57d420e5906f4d6bd16153b2a1c4a83524
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-26 16:53:14 +0100

    Searching through association - Joins working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -25,3 +25,38 @@ class MoviesController < ApplicationController
     end
   end
 end
+
+
+#Searching multiple columns - Or
+#The term might be in the movie's title or in its syllabus...
+
+
+class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR syllabus ILIKE :query"
+      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
+  end
+end
+
+#Searching through association - Joins
+#The term might also be in the director's name:
+
+class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      sql_query = " \
+        movies.title ILIKE :query \
+        OR movies.syllabus ILIKE :query \
+        OR directors.first_name ILIKE :query \
+        OR directors.last_name ILIKE :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
+  end
+end
