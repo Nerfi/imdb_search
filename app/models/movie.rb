50c956dabe9309c45df28f2afffbf7afd47f11b6
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-26 17:19:12 +0100

    searching through association kinda working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,3 +8,21 @@ class Movie < ApplicationRecord
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
 end
+
+#Searching through association
+#We want chris nolan to return Nolan's movies:
+
+
+
+class Movie < ApplicationRecord
+  belongs_to :director
+  include PgSearch
+  pg_search_scope :global_search,
+    against: [ :title, :syllabus ],
+    associated_against: {
+      director: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+end
